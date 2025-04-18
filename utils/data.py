25f0e632fcd74e17384e3d7a8cdca1315254cd0c
Viktor Kocur <kocurvik@gmail.com>
2025-04-18 20:32:15 +0200

    change vis

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 
 experiments = ['Feq_7pt', 'Feq_7pt_s3', 'kFk_8pt', 'kFk_9pt', 'k2k1_9pt', 'k2Fk1_10pt', 'F_7pt', 'F_7pt_s3', 'Fns_7pt']
-iterations_list = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+iterations_list = [10, 20, 50, 100, 200, 500, 1000]
 colors = {exp: sns.color_palette("tab10")[i] for i, exp in enumerate(experiments)}
 colors[experiments[7]] = sns.color_palette("tab10")[9]
 # basenames_pt = ['brandenburg_gate', 'buckingham_palace', 'colosseum_exterior', 'grand_place_brussels',
